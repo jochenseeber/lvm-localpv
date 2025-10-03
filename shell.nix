cae4d74242dc0b76f2577496986c918d2514ff19
Jochen Seeber <jochen@seeber.me>
2025-10-03 10:44:35 +0000

    chore: make develop branch build again
    - Some dependencies required Go 1.23, but project was configured for Go 1.19
    - Integration tests were broken
    - Updated Go to 1.24.7
    - Required some follow up changes to make everything run again
        - Updated golangci-lint to 2.5.0
        - Updated k8s modules to 0.34.1
        - Updated code generation to write to the correct location
        - Updated Nix configuration
    - CI script changes:
        - Added own fio container as there was no arm64 support
        - Added a provider option to select k3s
        - Added a data directory option because tests were failing on `/tmp`
        - Made script run as non-root user with sudo permission
    - Updated Nix configuration to use Go 1.24.7
    - Fixed Go lint errors
    - Added a Vagrantfile to run a development VM for non-Linux users
    
    Signed-off-by: Jochen Seeber <jochen@seeber.me>

--- shell.nix
+++ shell.nix
@@ -7,7 +7,8 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     chart-testing
     git
-    go_1_19
+    go_1_24
+    golangci-lint
     golint
     kubectl
     kubernetes-helm
@@ -24,29 +25,45 @@ pkgs.mkShell {
     jq
     lvm2_dmeventd
     nixos-shell
+    niv
   ] ++ pkgs.lib.optional (builtins.getEnv "IN_NIX_SHELL" == "pure") [ docker-client ];
 
   PRE_COMMIT_ALLOW_NO_CONFIG = 1;
 
   shellHook = ''
+    unset GOARCH
+    unset GOOS
     unset GOROOT
-    export GOPATH=$(pwd)/nix/.go
-    export GOCACHE=$(pwd)/nix/.go/cache
-    export TMPDIR=$(pwd)/nix/.tmp
-    export PATH=$GOPATH/bin:$PATH
+    
+    # Temp directories should not be in project directory to avoid issues if it's mounted remotely
+    export CGO_ENABLED=0
+    export GOCACHE="$HOME/nix/.go/.cache"
+    export GOENV=off
+    export GOMODCACHE="$HOME/nix/.go/.modcache"
+    export GOPATH="$HOME/nix/.go"
+    export GOPROXY=direct
+    export GOTELEMETRY="off"
+    export GOTMPDIR="$HOME/nix/.go/.tmp"
+    export GOTOOLCHAIN=local
+    export PATH="$GOPATH/bin:$PATH"
+    export TMPDIR="$HOME/nix/.tmp"
+
+    mkdir -p "$GOCACHE"
+    mkdir -p "$GOMODCACHE"
+    mkdir -p "$GOTMPDIR"
     mkdir -p "$TMPDIR"
 
     if [ "$IN_NIX_SHELL" = "pure" ]; then
       # working sudo within a pure nix-shell
       for sudo in /run/wrappers/bin/sudo /usr/bin/sudo /usr/local/bin/sudo /sbin/sudo /bin/sudo; do
-        mkdir -p $(pwd)/nix/bins
-        ln -sf $sudo $(pwd)/nix/bins/sudo
-        export PATH=$(pwd)/nix/bins:$PATH
+        mkdir -p $HOME/nix/bins
+        ln -sf $sudo $HOME/nix/bins/sudo
+        export PATH=$HOME/nix/bins:$PATH
         break
       done
     else
-      rm $(pwd)/nix/bins/sudo 2>/dev/null || :
-      rmdir $(pwd)/nix/bins 2>/dev/null || :
+      rm $HOME/nix/bins/sudo 2>/dev/null || :
+      rmdir $HOME/nix/bins 2>/dev/null || :
     fi
 
     make bootstrap
